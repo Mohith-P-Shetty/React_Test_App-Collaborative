5a640b565f10e1a75e444bb02792a33397375fff
Mohith p Shetty <156646298+Mohith-P-Shetty@users.noreply.github.com>
2024-12-16 10:49:30 +0530

    Update AppRoutes.jsx

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -6,14 +6,13 @@ import AdminLogin from "../pages/AdminLogin";
 import TestPage from "../pages/TestPage";
 import ResultPage from "../pages/ResultPage";
 import AdminPage from "../pages/AdminPage";
-import AddQuestion from "../AddQuestion";
+
 
 function AppRoutes() {
   return (
     <div>
       <Routes>
         <Route path="/" element={<LandingPage />}></Route>
-        <Route path="/add" element={<AddQuestion />}></Route>
         <Route path="/adminlogin" element={<AdminLogin />}></Route>
         <Route path="/candidatelogin" element={<CandidateLogin />}></Route>
         <Route path="/testpage" element={<TestPage />}></Route>
